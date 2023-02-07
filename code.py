f54c4525b317d1341c5f0ce51250b53adf9d7cbf
Laura-Boutonnet <62285953+Laura-Boutonnet@users.noreply.github.com>
2023-02-07 11:52:57 +0100

    Create code.py

---
+++ code.py
@@ -0,0 +1,7 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as f
+from torch.utils.data.dataloader import DataLoader
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
+import os
